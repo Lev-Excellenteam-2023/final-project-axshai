bababda0bc96a8483c1298c293b452b738c8a22a
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-05-18 15:28:12 +0300

    openai_client: Add get_slide_explanation func
    this function will handle the actual request to
    openai (using _set_prompt func)

--- openai_client.py
+++ openai_client.py
@@ -8,3 +8,29 @@ class OpenAiClient:
         openai.api_key = api_key
         self._max_tokens = 200
         self._model = "text-davinci-003"
+
+    @classmethod
+    def _set_prompt(cls, slide_txt):
+        """
+        returns a prompt to send to openai based on a given slide tex
+        :param slide_txt: the text of the slide
+        :return: the prompt
+        """
+
+        return f"""
+        Here is text extracted from a slide of a lecture presentation.
+        explain (and elaborate when needed) the content of each slide in a concise and coherent manner.
+        So that even a student who was not in the lecture will be able to understand the topic being discussed.
+
+        The text of the slide:
+        {slide_txt}
+        
+        Explanation of the topic following the text in the slide (6 - 7 sentences):
+        """
+
+    def get_slide_explanation(self, slide_txt):
+        response = openai.Completion.create(
+            model=self._model,
+            prompt=self._set_prompt(slide_txt),
+            max_tokens=self._max_tokens)
+        return response["choices"][0]["text"]
