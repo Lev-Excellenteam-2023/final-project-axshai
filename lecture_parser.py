95c6a671def775d4c44370b14875e29472a079f2
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-05-18 13:33:10 +0300

    lecture_parser: Add PresentationParser
    PresentationParser is one implmention
    to LectureParser.

--- lecture_parser.py
+++ lecture_parser.py
@@ -1,3 +1,6 @@
+from pptx import Presentation, slide
+
+
 class LectureParser:
     def __init__(self, lecture_path):
         self._lecture = lecture_path
@@ -7,3 +10,25 @@ class LectureParser:
 
     def parse_lecture_part(self, lecture_part) -> str:
         raise NotImplementedError
+
+
+class PresentationParser(LectureParser):
+    def __init__(self, lecture_path):
+        super().__init__(lecture_path)
+        self._parser = Presentation(self._lecture)
+
+    def get_lecture_parts(self):
+        for pres_slide in self._parser.slides:
+            yield pres_slide
+
+    def parse_lecture_part(self, pres_slide: slide.Slide) -> str:
+        # text_runs will be populated with a list of strings,
+        # one for each text run in presentation
+        text_runs = []
+        for shape in pres_slide.shapes:
+            if not shape.has_text_frame:  # TODO - extract text also from pictures
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                for run in paragraph.runs:
+                    text_runs.append(run.text)
+        return " ".join(text_runs)
