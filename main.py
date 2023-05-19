adbaf10922a2e8549e913ce03a27c1e17f8f7b68
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-05-19 09:51:07 +0300

    main: refactor - move code to AppEngine
    since the responsibility for running the
     application was transferred to AppEngine.

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import argparse
-import os
-import openai_client
-from dotenv import dotenv_values
-from lecture_parser import lecture_factory
-import json
 
-ENV_FILE_PATH = ".env"
-OPENAI_API_KEY = "OPENAI_API_KEY"
+from lecture_app_engine import AppEngine
 
 
 def _parse_args():
@@ -21,42 +15,10 @@ def _parse_args():
     return parser.parse_args()
 
 
-def _get_lecture_name_and_type(lecture_path: str) -> tuple[str, str]:
-    return tuple(os.path.basename(lecture_path).split("."))
-
-
-def _save_explained_lecture(explained_lecture_parts, lecture_path, destination_path, lecture_name):
-    dir_path = destination_path if destination_path else lecture_path
-    file_path = os.path.join(dir_path, lecture_name + ".json")
-    with open(file_path, 'w') as json_file:
-        json.dump(explained_lecture_parts, json_file)
-
-
-def explain_lecture(lecture_parser):
-    explained_lecture_parts = []
-    api_key = dotenv_values(ENV_FILE_PATH)[OPENAI_API_KEY]
-    client = openai_client.OpenAiClient(api_key)
-
-    for part in lecture_parser.get_lecture_parts():
-        try:
-            text_of_lec_part = lecture_parser.parse_lecture_part(part)
-            part_explanation = client.get_slide_explanation(text_of_lec_part)
-        except Exception as e:
-            part_explanation = f"An error occurred during Processing this part: {str(e)}"
-        finally:
-            if part_explanation:
-                explained_lecture_parts.append(part_explanation)
-
-    return explained_lecture_parts
-
-
 def main():
     args = _parse_args()
-    lecture_name, lecture_type = _get_lecture_name_and_type(args.lecture_path)
-    lecture_parser = lecture_factory(lecture_type)
-    explained_lecture_parts = explain_lecture(lecture_parser)
-    _save_explained_lecture(explained_lecture_parts, os.path.dirname(args.lecture_path),
-                            args.destination_path, lecture_name)
+    app_runner = AppEngine(args.lecture_path, args.destination_path)
+    app_runner.run_app()
 
 
 if __name__ == "__main__":
